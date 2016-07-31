4208ba301e8fe0b2457459473bee53ec15ddbbe6
Bill Hayden <bill.hayden@avimark.net>
2016-07-31 16:39:49 -0500

    create process done

--- tests/controllers/gameController.spec.js
+++ tests/controllers/gameController.spec.js
@@ -1,25 +1,41 @@
 describe('game controller', function() {
     var vm;
     var mockDb = {
-        getDocument: function() {}
+        getDocument: function() {},
+        updateDocument: function() {}
     };
     var scope;
     var q;
+    var game = {
+        _id: 1,
+        name: 'match',
+        _rev: 4, 
+        aspects: [{
+            appliesTo: 'player1',
+            desc: 'Ho hum'
+        },
+        {
+            appliesTo: 'player1',
+            desc: 'Yo hum'
+        },
+        {
+            appliesTo: 'environment',
+            desc: 'Ruh roh'
+        }]
+    }
 
     beforeEach(function() {
         angular.mock.module('fate');
         angular.mock.module('fateControllers');
         inject(function($controller, $q, $rootScope) {
             scope = $rootScope;
-            spyOn(mockDb, 'getDocument')
-                .and.returnValue(
-                    $q.when({_id: 1, name: 'match'})
-                );
-            q = $q;
+            spyOn(mockDb, 'getDocument').and.returnValue($q.when(game));
+            spyOn(mockDb, 'updateDocument');
             vm = $controller('gameController', {
                 gameId: 1,
                 isDm: true,
-                db: mockDb
+                db: mockDb,
+                $rootScope: scope
             });
         });
     });
@@ -36,8 +52,29 @@ describe('game controller', function() {
         expect(mockDb.getDocument).toHaveBeenCalledWith(1)
     });
 
+    it('should set title to game name', function() {
+        scope.$apply();
+        expect(vm.title).toBe(game.name);
+    })
+
     it('should set game to game from db', function() {        
         scope.$apply();
         expect(vm.game.name).toBe("match");
     });
+
+    it('should group the aspects', function() {
+        scope.$apply();
+        expect(vm.groupedAspects.player1.length).toBe(2);
+    });
+
+    describe('add', function() {
+        it('should call update document', function() {
+            scope.$apply();
+            vm.description = 'new aspect';
+            vm.appliesTo =  'player2';
+            vm.add();
+            game.aspects.push({description: 'new aspect', appliesTo: 'player2'});
+            expect(mockDb.updateDocument).toHaveBeenCalledWith(1, 4, game);
+        });
+    })
 })
--- www/app/game/gameController.js
+++ www/app/game/gameController.js
@@ -5,14 +5,67 @@
         .module('fateControllers')
         .controller('gameController', gameController);
 
-    gameController.$inject = ['db', 'gameId', 'isDm'];
-    function gameController(db, gameId, isDm) {
+    gameController.$inject = ['db', 'gameId', 'isDm', '$rootScope'];
+    function gameController(db, gameId, isDm, $rootScope) {
         var vm = this;
         vm.isDm = isDm;
+        vm.add = add;
         
-        db.getDocument(gameId).then(function(game) {
-            console.log(game);
+        getGame()
+            .then(setTitle)
+            .then(listenToChange);
+
+        function getGame() {
+            return db.getDocument(gameId)
+                .then(setGame)
+                .then(groupAspects);
+        }
+
+        function setGame(game) {
             vm.game = game;
-        });
+            return game;
+        }
+
+        function setTitle(game) {
+            vm.title = game.name;
+            return game;
+        }
+
+        function groupAspects(game) {
+            vm.groupedAspects =
+            game.aspects.reduce(function(source, aspect) {
+                source[aspect.appliesTo] = source[aspect.appliesTo] || [];
+                source[aspect.appliesTo].push(aspect);
+                return source;
+            }, {});
+            return game;
+        }
+
+        function add() {
+            var aspect = Aspect(vm.description, vm.appliesTo);
+            vm.game.aspects.push(aspect);
+            //TODO - do we need to listen for changes to update aspects
+            //maybe in player mode only?
+            db.updateDocument(vm.game._id, vm.game._rev, vm.game);
+        }
+
+        function listenToChange() {
+            $rootScope.$on("couchbase:change", function(event, args) {
+                var matchingResults = args.results.filter(function(result) {
+                    return result.id === gameId;
+                });
+
+                if(matchingResults.length > 0) {
+                    getGame();
+                }
+            });
+        }
+    }
+
+    function Aspect(description, appliesTo) {
+        return {
+            description: description,
+            appliesTo: appliesTo || 'environment',
+        };
     }
 })();
